189d3883e3b71a91c525e7511c0e45495cecf953
Finfingway <shiruha0915nishi@gmail.com>
2025-11-26 21:10:28 +0900

    Update P3OversampledWaveCannon.cs

--- BossMod/Modules/Endwalker/Ultimate/TOP/P3OversampledWaveCannon.cs
+++ BossMod/Modules/Endwalker/Ultimate/TOP/P3OversampledWaveCannon.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using BossMod;
-using BossMod.Components;
-
 namespace BossMod.Endwalker.Ultimate.TOP
 {
-    // P3 Oversampled Wave Cannon の散会処理
     public sealed class P3OversampledWaveCannon : BossModule
     {
         private readonly Dictionary<int, WPos> _safeSpots = new();
@@ -15,10 +8,15 @@ namespace BossMod.Endwalker.Ultimate.TOP
         private static readonly int MT = 0, OT = 1, H1 = 2, H2 = 3, M1 = 4, M2 = 5, R1 = 6, R2 = 7;
 
         public P3OversampledWaveCannon(WorldState ws, Actor primary)
-            : base(ws, primary, new(100, 100), new ArenaBoundsCircle(30)) { }
+            : base(ws, primary, new(100, 100), new ArenaBoundsCircle(30))
+        {
+            ws.OnStatusGain += HandleStatusGain;
+            ws.OnStatusLose += HandleStatusLose;
+        }
 
-        public override void OnStatusGain(Actor actor, ref ActorStatus status) => UpdateAssignment();
-        public override void OnStatusLose(Actor actor, ref ActorStatus status) => UpdateAssignment();
+        // Monitor 状態変化時に呼ばれる
+        private void HandleStatusGain(Actor actor, ref ActorStatus status) => UpdateAssignment();
+        private void HandleStatusLose(Actor actor, ref ActorStatus status) => UpdateAssignment();
 
         private void UpdateAssignment()
         {
@@ -36,7 +34,7 @@ namespace BossMod.Endwalker.Ultimate.TOP
             _safeSpots.Clear();
             var center = Arena.Center;
 
-            // 散会座標オフセット
+            // N/E/S/W 散会オフセット
             var offsets = new WDir[4] {
                 new WDir(0, +11),  // 北
                 new WDir(+11, 0),  // 東
@@ -51,7 +49,8 @@ namespace BossMod.Endwalker.Ultimate.TOP
             }
         }
 
-        public override void DrawArenaForeground(int pcSlot, Actor pc)
+        // 描画用
+        protected override void DrawArenaForeground(int pcSlot, Actor pc)
         {
             foreach (var kv in _safeSpots)
             {
@@ -60,14 +59,14 @@ namespace BossMod.Endwalker.Ultimate.TOP
             }
         }
 
-        // Monitor/Normal 散会決定ルール
+        // Monitor/Normal 散会割り当て
         private class MonitorAssignment
         {
             public readonly int[] position = new int[8];
 
             public MonitorAssignment(bool[] monitor)
             {
-                // 初期散会: N=H1, MT / E=OT, H2 / S=M2, R2 / W=M1, R1
+                // 初期散会: N/E/S/W
                 position[MT] = 0; position[H1] = 0;
                 position[OT] = 1; position[H2] = 1;
                 position[M2] = 2; position[R2] = 2;
@@ -78,7 +77,6 @@ namespace BossMod.Endwalker.Ultimate.TOP
 
             private void Adjust(bool[] monitor)
             {
-                // THグループ(MT,OT,H1,H2) / DPSグループ(M1,M2,R1,R2)
                 int[] TH = { MT, OT, H1, H2 };
                 int[] DPS = { M1, M2, R1, R2 };
 
@@ -92,29 +90,21 @@ namespace BossMod.Endwalker.Ultimate.TOP
 
                 switch (monitorCount)
                 {
-                    case 0: return; // 調整なし
+                    case 0: return;
                     case 1:
-                        // 横軸に1人になるようにペア交代
                         int solo = group.First(i => monitor[i]);
                         int pair = group.First(i => !monitor[i]);
                         Swap(solo, pair);
                         break;
                     case 2:
-                        // 横1縦1になるよう調整
                         int[] monitors = group.Where(i => monitor[i]).ToArray();
                         if (position[monitors[0]] == position[monitors[1]])
-                        {
-                            // 同軸に並んでいる場合、近接同士を交代
                             Swap(monitors[0], group.First(i => !monitor[i]));
-                        }
                         break;
                     case 3:
-                        // 縦軸2人の場合は検知0人のペアと交代
                         int[] nonMonitors = group.Where(i => !monitor[i]).ToArray();
                         if (nonMonitors.Length == 1)
-                        {
                             Swap(nonMonitors[0], group.First(i => monitor[i] && position[i] != position[nonMonitors[0]]));
-                        }
                         break;
                 }
             }
